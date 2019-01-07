95527ce3f754d9b5142199b729c44b00c781cd29
jabiz <daniel.jabore@hotmail.com>
2019-01-07 17:23:03 +0000

    inital commit on the monday branch

--- src/main/scala/scalaExercises.scala
+++ src/main/scala/scalaExercises.scala
@@ -1,14 +1,65 @@
-class scalaExercises
-{
+import scala.math
+object scalaExercises {
+  def checker() {
+    for (i <- 100000000 to 300000000 if primechecker(i) == true) {
+      println(i)
+    }
+  }
+def primechecker(i:Int):Boolean=
+  {
+   if(i%2 !=0) {
+     if (i % 3 != 0)
+     {
+        if(i % 5 !=0)
+          {
+            if(i % 7 !=0)
+              {
+               if(i % 11 !=0)
+                 {
+                   if(i % 13 !=0)
+                     {
+                       if(math.round((math.sqrt(i))) % i !=0 )
+                         {
+                           true
+                         }
+                       else
+                         {
+                           false
+                         }
+                     }
+                   else
+                     {
+                       false
+                     }
+                 }
+                else
+                 {
+                   false
+                 }
+              }
+            else
+            {
+              false
+            }
+          }
+        else
+          {
+            false
+          }
+     }
+     else {
+        false
+     }
+   }
+    else{
+        false
+    }
+  }
 
-
-for(i<1000000 to 3000000 if )
-
-def
 
 
   def main(args: Array[String]): Unit =
   {
-
+  checker()
   }
 }
